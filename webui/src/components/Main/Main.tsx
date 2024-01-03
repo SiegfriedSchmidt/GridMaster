6536cfa8abf0e2b1189e0b4f70b6ab47c879bd05
Matvei <matveiyan@yandex.ru>
2024-01-04 00:51:24 +0300

    Import and exporting files added

--- webui/src/components/Main/Main.tsx
+++ webui/src/components/Main/Main.tsx
@@ -1,20 +1,20 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {ChangeEvent, MouseEventHandler, useEffect, useRef, useState} from 'react';
 import classes from "./Main.module.css";
 import Executor from "../Executor/Executor";
 import {compileCode} from "../../api/compileCode";
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, {ReactCodeMirrorRef} from '@uiw/react-codemirror';
 import Slider from "../Slider/Slider";
 
 function setStartStyles(buttonRef: React.RefObject<HTMLButtonElement>) {
     if (buttonRef.current) {
-        buttonRef.current.style.backgroundColor = 'greenyellow'
+        buttonRef.current.classList.remove(classes.buttonStop)
         buttonRef.current.innerText = 'Run'
     }
 }
 
 function setStopStyles(buttonRef: React.RefObject<HTMLButtonElement>) {
     if (buttonRef.current) {
-        buttonRef.current.style.backgroundColor = 'orangered'
+        buttonRef.current.classList.add(classes.buttonStop)
         buttonRef.current.innerText = 'Stop'
     }
 }
@@ -23,6 +23,7 @@ const Main = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const executorRef = useRef<Executor>()
     const buttonRef = useRef<HTMLButtonElement>(null)
+    const codeMirrorRef = useRef<ReactCodeMirrorRef>(null)
     const [delay, setDelay] = useState<number>(100)
     const [code, setCode] = useState<string>('')
 
@@ -57,20 +58,56 @@ const Main = () => {
         }
     }
 
+
+    async function importFile(e: ChangeEvent<HTMLInputElement>) {
+        let file = e.target.files![0];
+        let reader = new FileReader();
+        reader.readAsText(file);
+        e.target.value = ''
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+                setCode(reader.result)
+            } else {
+                alert(`Error file reading: ${reader.error}`)
+            }
+        }
+        reader.onerror = () => {
+            alert(`Error file reading: ${reader.error}`)
+        }
+    }
+
+    function downloadFile(filename: string, text: string) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+
+    async function exportFile() {
+        downloadFile('Program.txt', code)
+    }
+
     return (
         <div className={classes.container}>
             <div className={classes.settings}>
                 <input className={classes.checkbox} type="checkbox"
                        onChange={e => executorRef.current?.drawing_mode(e.target.checked)}/>
                 <Slider setValue={setDelay}/>
+                <input id='import' type="file" onChange={importFile} style={{display: "none"}}/>
+                <label className={classes.file} htmlFor="import">Import</label>
+                <button className={classes.button} onClick={exportFile}>Export</button>
             </div>
 
             <div className={classes.textExecContainer}>
                 <CodeMirror
+                    ref={codeMirrorRef}
                     onChange={value => setCode(value)}
                     className={classes.code}
                     placeholder='Пишите, что хотите'
-                    value=""
+                    value={code}
                     height="40vh"
                     theme="light"
                 />
@@ -78,7 +115,6 @@ const Main = () => {
             </div>
             <div className={classes.buttonsContainer}>
                 <button style={{}} className={classes.button} ref={buttonRef} onClick={onClick}>Run</button>
-                <input type="file" onChange={e => 1}/>
             </div>
         </div>
     );
